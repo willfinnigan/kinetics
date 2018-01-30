f56ddea8553e450e810bca94d51c6d811f323775
William Finnigan <wjafinnigan@gmail.com>
2018-01-30 22:14:11 +0000

    cleaned up some formatting

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='Kinetics',  # Required
-    version='1.1',  # Required
+    version='1.2',  # Required
     description='Python code to run kinetic models of enzyme reactions',
     url='https://github.com/willfinnigan/Kinetics',
     author='William Finnigan',
