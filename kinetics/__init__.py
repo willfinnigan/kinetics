84c5d3f5db43a0083aec1342fe2f74f13a26c240
William Finnigan <wjafinnigan@gmail.com>
2018-04-05 16:57:56 +0100

    Updated the example, added graph functions

--- kinetics/__init__.py
+++ kinetics/__init__.py
@@ -6,3 +6,5 @@ from kinetics.Equations import *
 
 from kinetics.Uncertainty_Sensitivity_Analysis import UA, SA, get_bounds_from_pc_error, get_bounds_from_std_error
 
+from kinetics.graph_functions import setup_ua_graph, add_ua_model_to_graph, add_experimental_data_to_graph, remove_st_less_than, plot_sa_total_sensitivity, set_graph_settings
+
