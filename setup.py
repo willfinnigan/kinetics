bf4ee552d69d57a344b0590401bb181d95ee9cbf
William Finnigan <wjafinnigan@gmail.com>
2019-03-16 15:10:24 +0000

    Moved to v1.1 for dev branch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'kinetics',
   packages = ['kinetics'],
-  version = '1.0',
+  version = '1.1',
   license='MIT',
   description = 'Python code to run kinetic models of enzyme reactions',
   author = 'William Finnigan',
