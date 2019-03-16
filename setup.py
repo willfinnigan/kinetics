04425e83cafe646c4a0f05ca598430bad0ff8f41
William Finnigan <wjafinnigan@gmail.com>
2019-03-16 14:57:03 +0000

    Changes for uploading to pypi

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from setuptools import setup, find_packages
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-setup(
-    name='kinetics',  # Required
-    version='1.00',  # Required
-    description='Python code to run kinetic models of enzyme reactions',
-    url='https://github.com/willfinnigan/kinetics',
-    author='William Finnigan',
-    author_email='wjafinnigan@gmail.com',
-    keywords='enzyme kinetics modelling',
-    packages=find_packages())
-
-
 from distutils.core import setup
 setup(
   name = 'kinetics',
