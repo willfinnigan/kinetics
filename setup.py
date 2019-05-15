e0a671a76134e8adf15d3444f52fa5ec5d520b1d
William Finnigan <wjafinnigan@gmail.com>
2019-05-15 14:56:43 +0100

    Changes to the way ga metrics are calculated

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'kinetics',
   packages = ['kinetics'],
-  version = '1.1.4',
+  version = '1.1.5',
   license='MIT',
   description = 'Python code to run kinetic models of enzyme reactions',
   author = 'William Finnigan',
   author_email = 'wjafinnigan@gmail.com',
   url = 'https://github.com/willfinnigan/kinetics',
-  download_url = '',
+  download_url = 'https://github.com/willfinnigan/kinetics/archive/1.1.4.tar.gz',
   keywords = ['enzyme', 'kinetics', 'modelling'],
   install_requires=['scipy', 'numpy', 'SALib', 'tqdm', 'matplotlib', 'pandas', 'deap'],
   classifiers=[
