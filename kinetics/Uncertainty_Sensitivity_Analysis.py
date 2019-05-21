bb57414083cabd346b9f08751efa5d77ed759769
William Finnigan <wjafinnigan@gmail.com>
2019-05-21 12:43:46 +0100

    first take on check parameter limits

--- kinetics/Uncertainty_Sensitivity_Analysis.py
+++ kinetics/Uncertainty_Sensitivity_Analysis.py
@@ -408,6 +408,8 @@ class UA(object):
                                      self.species_names, self.species_bounds)
 
         self.samples = random_sample(self.problem, self.num_samples)
+        
+        self.samples = self.check_parameter_limits()
 
         self.parsed_samples = parse_samples_to_run(self.samples, self.parameter_names, self.species_names)
 
@@ -428,7 +430,7 @@ class UA(object):
 
         if self.logging == True:
             print('Set of samples which passed = ' + str(len(self.samples)))
-        
+
 
     def run_models(self):
         if self.logging == True:
