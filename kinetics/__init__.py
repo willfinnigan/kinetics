9c55841f673e9d2cb5a7b74f67bc362aa6f1ef59
William Finnigan <wjafinnigan@gmail.com>
2019-08-22 11:56:16 +0100

    1.3.8

--- kinetics/__init__.py
+++ kinetics/__init__.py
@@ -15,4 +15,4 @@ from kinetics.ua_and_sa.run_all_models import run_all_models, dataframes_all_run
 from kinetics.ua_and_sa.plotting import plot_substrate, plot_ci_intervals, plot_data, remove_st_less_than, plot_sa_total_sensitivity
 from kinetics.ua_and_sa.sensitivity_analysis import get_concentrations_at_timepoint, get_time_to_concentration, analyse_sobal_sensitivity
 
-__version__ = '1.3.7.1'
+__version__ = '1.3.8'
