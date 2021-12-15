a37a9e3ae7ede58dbb4557c5bebad98fbc2a7a2d
willfinnigan <wjafinnigan@gmail.com>
2021-12-15 14:51:45 +0000

    adds some equilibrium based rate laws

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import setuptools
 from distutils.core import setup
 
 setup(
@@ -7,13 +8,14 @@ setup(
               'kinetics.other_analysis',
               'kinetics.reaction_classes',
               'kinetics.ua_and_sa'],
+  setup_requires=['wheel'],
   version = '1.4.2',
   license='MIT',
   description = 'Python code to run kinetic models of enzyme reactions',
   author = 'William Finnigan',
   author_email = 'wjafinnigan@gmail.com',
   url = 'https://github.com/willfinnigan/kinetics',
-  download_url = 'https://github.com/willfinnigan/kinetics/archive/1.4.1.tar.gz',
+  download_url = 'https://github.com/willfinnigan/kinetics/archive/1.4.2.tar.gz',
   keywords = ['enzyme', 'kinetics', 'modelling'],
   install_requires=['scipy', 'numpy', 'SALib', 'tqdm', 'matplotlib', 'pandas', 'deap', 'seaborn'],
   classifiers=[
